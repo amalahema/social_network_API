d497c11884bb94a3aa392c4cbb0c7d805fd092b5
Amala Hema <amalahema@gmail.com>
2023-05-26 01:33:47 -0400

    middleware top functions express.json() from framework express assigned to variable

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 //pass the functioality of express to the variable through require bulid in function in nodejs
 const express = require('express');
-const { MongoClient } = require('mp')
+
+//get the details of database through mongoose connection object from the config file 
+const db = require('./config/connection');
+const { urlencoded } = require('body-parser');
+
+//require model
+
+//port determines the port on which server listens
+const PORT = process.env.PORT|| 3001;
+
+//app defines the server behavior
+const app = (express());
+
+//html user  data format paresd to js objest
+app.use(express.urlencoded({extended: true}));
+//js object json and amle it available in req.body
+app.use(express.json);
